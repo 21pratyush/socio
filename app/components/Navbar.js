ddc7f72b036a451f3babcd897f42f5b7ba93e3e9
sagardev09 <sagardbs8121@gmail.com>
2023-09-18 15:07:11 +0530

    side bar completed

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,8 +1,43 @@
+import Image from 'next/image'
 import React from 'react'
+import search from "@/public/Svg/search.svg"
+import avtar from "@/public/Pics/avtar.avif"
 
 const Navbar = () => {
     return (
-        <div className='w-full h-[8vh] bg-black'>Navbar</div>
+        <div className='w-full h-[8vh] bg-neutral-100 border-b border-b-gray-400 border-opacity-30 flex '>
+            <div className='w-[70%] flex justify-end items-center space-x-8'>
+                <div className='border-t-[4px] border-t-blue-500 h-full flex items-center px-3 w-fit '>
+                    <h5>Home</h5>
+                </div>
+                <div className='border-t-[4px] border-t-blue-500 h-full flex items-center px-3 w-fit '>
+                    <h5>Notification</h5>
+                </div>
+                <div className='border-t-[4px] border-t-blue-500 h-full flex items-center px-3 w-fit '>
+                    <h5>Watch</h5>
+                </div>
+                <div className='border-t-[4px] border-t-blue-500 h-full flex items-center px-3 w-fit '>
+                    <h5>Marketplace</h5>
+                </div>
+                <div className='border-t-[4px] border-t-blue-500 h-full flex items-center px-3 w-fit '>
+                    <h5>Groups</h5>
+                </div>
+                <div className='border-t-[4px] border-t-blue-500 h-full flex items-center px-3 w-fit '>
+                    <h5>Messenger</h5>
+                </div>
+                <div className='border-t-[4px] border-t-blue-500 h-full flex items-center px-3 w-fit '>
+                    <h5>
+                        Live
+                    </h5>
+                </div>
+            </div>
+            <div className=' w-[30%] flex justify-end items-center space-x-5 pr-8'>
+                <Image src={search} alt='searchicon' className='h-[30px] w-[30px] object-contain cursor-pointer' />
+                <div className='h-[50px] w-[50px] border-2 border-blue-600 rounded-full   overflow-hidden'>
+                    <Image src={avtar} alt='searchicon' className='h-[100%] w-[100%] cursor-pointer object-cover rounded-full ' />
+                </div>
+            </div>
+        </div>
     )
 }
 
--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,17 +1,38 @@
 import Image from 'next/image'
 import React from 'react'
 import facebook from "@/public/Svg/facebook.svg"
+import add from "@/public/Svg/add.svg"
+import bookmark from "@/public/Svg/bookmark.svg"
+import calendar from "@/public/Svg/calendar.svg"
+import category from "@/public/Svg/category.svg"
+import help from "@/public/Svg/help.svg"
+import Like from "@/public/Svg/Like.svg"
+import play from "@/public/Svg/play.svg"
+import setting from "@/public/Svg/setting.svg"
+import videos from "@/public/Svg/videos.svg"
+
 
 const Sidebar = () => {
     return (
-        <div className='min-h-screen w-[8vh] bg-neutral-200'>
-            <div>
+        <div className='min-h-screen w-[8vh] bg-neutral-100 flex justify-between items-center flex-col py-3 border-r border-r-gray-400 border-opacity-30'>
+            <div className='flex flex-col justify-center space-y-8 items-center'>
                 <div>
-                    <Image src={facebook} alt='logo' className='' />
+                    <Image src={facebook} alt='logo' className='h-[52px] w-[52px] object-contain cursor-pointer' />
                 </div>
-                <div></div>
+                <div className='flex flex-col items-center gap-8'>
+                    <Image src={calendar} alt='logo' className='h-[38px] w-[38px] object-contain cursor-pointer' />
+                    <Image src={bookmark} alt='logo' className='h-[38px] w-[38px] object-contain cursor-pointer' />
+                    <Image src={play} alt='logo' className='h-[38px] w-[38px] object-contain cursor-pointer' />
+                    <Image src={Like} alt='logo' className='h-[38px] w-[38px] object-contain cursor-pointer' />
+                    <Image src={videos} alt='logo' className='h-[38px] w-[38px] object-contain cursor-pointer' />
+                    <Image src={help} alt='logo' className='h-[38px] w-[38px] object-contain cursor-pointer' />
+                    <Image src={setting} alt='logo' className='h-[38px] w-[38px] object-contain cursor-pointer' />
+                    <Image src={category} alt='logo' className='h-[38px] w-[38px] object-contain cursor-pointer' />
+                </div>
             </div>
-            <div></div>
+            <div>
+                <Image src={add} alt='logo' className='h-[60px] w-[60px] object-contain cursor-pointer' />
+            </div>
         </div>
     )
 }
